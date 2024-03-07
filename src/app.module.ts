84ff3f991f1a7caf9cf8507488f18091260f0aa9
Angel <angelgarcialopez15@gmail.com>
2024-03-07 14:25:02 +0100

    Subir archivos1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
 import { GamasModule } from './gamas/gamas.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductosModule, GamasModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging:false
+    })
+    ProductosModule, 
+    GamasModule],
   controllers: [AppController],
   providers: [AppService],
 })
